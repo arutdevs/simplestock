96a1c0211c5b196993c9e0e275efa5128c985bce
book <0arut.bun@gmail.com>
2025-10-14 00:25:43 +0700

    first commit

---
+++ src/app/features/products/product-list/product-list.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { Header } from '../../../shared/layout/header/header';
+
+@Component({
+  selector: 'app-product-list',
+  imports: [Header],
+  templateUrl: './product-list.html',
+  styleUrl: './product-list.scss',
+})
+export class ProductList {}
